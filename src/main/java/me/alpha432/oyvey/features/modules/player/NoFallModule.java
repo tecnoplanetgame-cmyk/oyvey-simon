5491893b6fc04149339dfa67a246d613fa5574bc
tecnoplanetgame-cmyk <tecnoplanetgame@gmail.com>
2026-05-16 15:43:43 +0200

    Update NoFallModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
@@ -1,8 +1,8 @@
 package me.alpha432.oyvey.features.modules.player;
-
 import me.alpha432.oyvey.OyVey;
 import me.alpha432.oyvey.features.modules.Module;
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
+import net.minecraft.tags.FluidTags;
 
 public class NoFallModule extends Module {
     public NoFallModule() {
@@ -11,10 +11,22 @@ public class NoFallModule extends Module {
 
     @Override
     public void onTick() {
+        // Se il giocatore è in acqua, non fare nulla → caduta normale
+        if (mc.player.isUnderWater() || mc.player.isInFluid(FluidTags.WATER)) {
+            return;
+        }
+
         if (!mc.player.onGround() && OyVey.positionManager.getFallDistance() > 3) {
             boolean bl = mc.player.horizontalCollision;
-            ServerboundMovePlayerPacket.PosRot packet = new ServerboundMovePlayerPacket.PosRot(mc.player.getX(), mc.player.getY() + 0.000000001, mc.player.getZ(),
-                    mc.player.getYRot(), mc.player.getXRot(), false, bl);
+            ServerboundMovePlayerPacket.PosRot packet = new ServerboundMovePlayerPacket.PosRot(
+                    mc.player.getX(),
+                    mc.player.getY() + 0.000000001,
+                    mc.player.getZ(),
+                    mc.player.getYRot(),
+                    mc.player.getXRot(),
+                    false,
+                    bl
+            );
             mc.player.connection.send(packet);
         }
     }
